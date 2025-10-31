e24a8a3070425c3007bb022b8d4bb4feb9622c19
conmefo <hoquangminh757@gmail.com>
2025-11-01 00:13:26 +0700

    first build

---
+++ minidrive/src/main/java/com/minidrive/MinidriveApplication.java
@@ -0,0 +1,13 @@
+package com.minidrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidriveApplication.class, args);
+	}
+
+}
---
+++ minidrive/src/test/java/com/minidrive/MinidriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minidrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
